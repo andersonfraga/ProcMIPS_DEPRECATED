9703d2311c4c8f80333450f82551abed874b1a97
Anderson Fraga <contato@andersonfraga.net>
2011-12-05 21:09:12 -0200

    Retirado código incorreto do teste via assembly-mars

--- testbench.vhd
+++ testbench.vhd
@@ -1,56 +1,8 @@
-----------------------------------------------------------------------------------
---                      Trabalho 2  (27/06/2011)
---Ivan Suptitz 67371
---Tailini Eugenio 65611
---Test Bench para verificação do funcionamento do processador MIPS Monociclo
---implementado. As insruções são carregadas de modo fixo sendo o código assemby a seguir:
-
-
--- 0: $s3 = $s3 + 10; (passa a ter 10)
--- 1: $s4 = $s4 + 1;  (passa a ter 1)
-
--- 2: $ra = $ra + 12; //esta é a posição 12 (4*3) voltará para cá
--- 3:   if $s3<$0 
-       -- $s5=1;
--- 4:   if $s5=$s4 
-       -- goto jump1;
-
--- 5:   $s6 = $s3<<2;
--- 6:   a[$s6] = $s6;
--- 7:   $s3 = $s3 - $s4;
--- 8: jump $ra;
-
--- jump1:
--- 9:  $t0 = 10 << 16;  (00000000000010100000000000000000 ou 655360 em decimal)
--- 10: $t1 = $t0 | 10;  (00000000000010100000000000001010 ou 655370 em decimal)
--- 11: $s0 = $t1 >> 16; (00000000000000000000000000001010 ou     10 em decimal)
--- 12: $s1 = $s1 + 1;
-
--- 13: $ra = $ra + 44; (passa a ter 56) //voltará para cá
--- 14:   if $s0<$0 
-        -- $t0=1;
--- 15:   if $to=$s1 
-        -- goto jump2;
-
--- 16:   $t3 = $s0<<2; (passa a ter 40)
--- 17:   $t4 = a[$t3]; (nesta posição de memória tem valor 40)
--- 18:   $t4 = $t4>>1; (passa a ter 00000000000000000000000000010100 ou 20 em decimal)
--- 19:   $t4 = $t4 + 1; (passa a ter 21)
--- 20:   a[$s0] = $t4;
--- 21:   $s0 = $S0 - $s1;
--- 22: jump $ra;
-
--- 23: jump2: //saída
-
-
-
-----------------------------------------------------------------------------------
-
   LIBRARY ieee;
   USE ieee.std_logic_1164.ALL;
   USE ieee.numeric_std.ALL;
   use ieee.std_logic_arith.all;
-  use work.p_MI0.ALL;  --pacote com as declarações utilizadas no processador
+  use work.p_MI0.ALL;  --pacote com as declaraï¿½ï¿½es utilizadas no processador
 
   ENTITY Processador_TB IS
   END Processador_TB;
@@ -58,11 +10,11 @@
   ARCHITECTURE behavior OF Processador_TB IS           
   signal Inst_in, Inst_address, posicaoMem, ula_op1, ula_op2, ula_out: reg32;
   signal Inst_load, Inst_read, clock, regWrite, memWrite: std_logic;
-  signal outDados, outInst: memory; -- para acompanhar a memória de dados e de instruções
+  signal outDados, outInst: memory; -- para acompanhar a memï¿½ria de dados e de instruï¿½ï¿½es
   signal regOut: bank; -- para acompanhar o banco de registradores
   
   BEGIN
-  -- Instanciação da UUT
+  -- Instanciaï¿½ï¿½o da UUT
           uut: entity work.Processador PORT MAP(
 				Inst_in=>Inst_in, Inst_address=>Inst_address,
 				Inst_load=>Inst_load, Inst_read=>Inst_read, clock=>clock,
@@ -82,7 +34,7 @@
      BEGIN
 	     
 		Inst_read<='0';
-		Inst_load<='1'; --começa a carga das instruções na memória
+		Inst_load<='1'; --comeï¿½a a carga das instruï¿½ï¿½es na memï¿½ria
 
 		Inst_address<= conv_std_logic_vector(0,32); 
 		Inst_in<=x"2673000A";   --00100110011100110000000000001010	
@@ -156,7 +108,7 @@
 		Inst_load<='0';--terminou de carregar		
 		Inst_read<='1';--vou resetar
 		wait for 10 ns;
-		Inst_read<='0';--começa
+		Inst_read<='0';--comeï¿½a
 
 		wait;
      END PROCESS tb;
